ad1715ac49f16f9b28f533a289afbec006de64a9
Mike Sperber <sperber@deinprogramm.de>
2022-01-24 09:54:31 +0100

    Update.

--- image.rkt
+++ image.rkt
@@ -10,3 +10,19 @@
 (define star1 (star 50 "solid" "gold"))
 
 (define overlay1 (overlay star1 circle1))
+
+(above
+ (beside circle1 star1)
+ (beside star1 circle1))
+
+(above
+ (beside square1 circle1)
+ (beside circle1 square1))
+
+; Abstraktion
+(define tile
+  (lambda (image1 image2)
+    (above
+     (beside image1 image2)
+     (beside image2 image1))))
+
