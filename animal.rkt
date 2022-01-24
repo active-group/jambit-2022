e4d8186e9e20298a634636491fa6c7235515c0d0
Mike Sperber <sperber@deinprogramm.de>
2022-01-24 13:38:15 +0100

    Update.

---
+++ animal.rkt
@@ -0,0 +1,9 @@
+;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
+;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
+#reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname animal) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
+; Tiere auf dem texanischen Highway
+
+; Ein Gürteltier hat folgende Eigenschaft:
+; - lebendig oder tot?
+; - Gewicht
+; zusammengesetzte Daten
