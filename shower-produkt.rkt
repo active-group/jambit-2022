4d8b4714730efce2cd529d3b93249aabdc96787b
Mike Sperber <sperber@deinprogramm.de>
2022-01-24 16:46:39 +0100

    Update.

---
+++ shower-produkt.rkt
@@ -0,0 +1,7 @@
+;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
+;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
+#reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname shower-produkt) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
+; Führe eine Datenanalyse durch für Duschprodukte!
+; Es gibt Seife, Shampoo und Duschgel.
+; bei Seife: pH-Wert
+; bei Shampoo: Farbe und Haartyp
