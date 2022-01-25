88c59822eaf627c20391b68f45e637f0df2bcd93
Mike Sperber <sperber@deinprogramm.de>
2022-01-25 15:02:35 +0100

    Update.

--- rev.rkt
+++ rev.rkt
@@ -1,3 +1,32 @@
 ;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
 #reader(lib "vanilla-reader.rkt" "deinprogramm" "sdp")((modname rev) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
+(: rev ((list-of %a) -> (list-of %a)))
+
+(check-expect (rev (list 1 2 3))
+              (list 3 2 1))
+
+(define rev
+  (lambda (list)
+    (cond
+      ((empty? list) empty)
+      ((cons? list)
+       (add-to-end
+        (rev (rest list))
+        (first list))))))
+
+; Element an Ende der Liste hängen
+(: add-to-end ((list-of %a) %a -> (list-of %a)))
+
+(check-expect (add-to-end (list 1 2 3) 4)
+              (list 1 2 3 4))
+
+(define add-to-end
+  (lambda (list el)
+    (cond
+      ((empty? list) (cons el empty))
+      ((cons? list)
+       (cons
+        (first list)
+        (add-to-end (rest list) el))))))
+  
