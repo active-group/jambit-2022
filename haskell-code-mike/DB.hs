b7af56c2c8514422b3f11271a8795e65c333a27e
Mike Sperber <sperber@deinprogramm.de>
2022-01-26 15:21:05 +0100

    Update.

--- haskell-code-mike/DB.hs
+++ haskell-code-mike/DB.hs
@@ -3,5 +3,23 @@ module DB where
 {-
 DSL für Datenbank-Programme:
 
-
+  put "Mike" 50
+  x = get "Mike"
+  put "Mike" (x + 1)
+  y = get "Mike"
+  return (show (x + y))
 -}
+
+{- -- FAIL: 
+
+data DBCommand result =
+    Put String Integer 
+  | Get String
+  | Return result
+
+type DBProgram result = [DBCommand result]
+
+p1 = [Put "Mike" 50, Get "Mike", Return "foo"]
+-- ^^^ können Dingen keinen Namen geben
+-}
+
